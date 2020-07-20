971097860929ffc4ecd6588038c2ae385fb9e055
Abhinav Manchanda <abhinav@abhinav.local>
2020-07-20 15:40:54 +0530

    Add parameters to upload to a custom path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 set -e
 
 [ -z $INPUT_AWS_S3_BUCKET_NAME ] && INPUT_AWS_S3_BUCKET_NAME="medly-dev-build-artifacts"
-
 [ -z $INPUT_AWS_REGION ] && INPUT_AWS_REGION="us-east-1"
 
 if [[ -z "$INPUT_AWS_ACCESS_KEY_ID" || \
@@ -17,7 +16,6 @@ if [[ -z "$INPUT_DIST_FILE_PATH" ]]; then
   exit 1
 fi
 
-
 aws configure --profile upload-artifacts-profile <<-EOF > /dev/null 2>&1
 ${INPUT_AWS_ACCESS_KEY_ID}
 ${INPUT_AWS_SECRET_ACCESS_KEY}
@@ -29,10 +27,18 @@ BRANCH_NAME=$(echo $GITHUB_REF | cut -d'/' -f 3)
 REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f 2)
 METADATA="{\\\"initiator\\\":\\\"$GITHUB_ACTOR\\\",\\\"commit_sha\\\":\\\"$GITHUB_SHA\\\"}"
 
-sh -c "aws s3 cp ${INPUT_DIST_FILE_PATH} s3://${INPUT_AWS_S3_BUCKET_NAME}/${REPO_NAME}/${BRANCH_NAME}/${GITHUB_RUN_NUMBER}.zip \
-              --profile upload-artifacts-profile \
-              --no-progress \
-              --metadata $METADATA" 
+
+if [[ -z "$DESTINATION_PATH" ]]; then
+  DESTINATION_PATH=${BRANCH_NAME}/${GITHUB_RUN_NUMBER}.zip	
+fi
+
+[[ $RESOURCE_TYPE = "DIRECTORY" ]] && ARGS=" -- recursive" || ARGS=""
+
+sh -c "aws s3 cp ${INPUT_DIST_FILE_PATH} s3://${INPUT_AWS_S3_BUCKET_NAME}/${REPO_NAME}/${DESTINATION_PATH} \
+        --profile upload-artifacts-profile \
+        --no-progress \
+        --metadata $METADATA \
+        ${ARGS} " 
 
 aws configure --profile upload-artifacts-profile <<-EOF > /dev/null 2>&1
 null
